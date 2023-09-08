2a0cabc12a68706016a421bba37a970451ac4f38
SalhiFayza <fayzasalhif@gmail.com>
2023-09-08 14:15:48 +0100

    add WebElemnet logout

--- src/test/java/logout/LogoutStepDefinition.java
+++ src/test/java/logout/LogoutStepDefinition.java
@@ -1,11 +1,30 @@
 package logout;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import Utils.CommonMethods;
+import Utils.Setup;
+import Utils.WaitMethods;
+import logout.LogoutPageObject;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class LogoutStepDefinition {
+    private WebDriver driver;
 	
+	private LogoutPageObject logoutPageObj = new LogoutPageObject();
+	 
+	private CommonMethods commenMotheds = new CommonMethods();
+	
+	private WaitMethods waitMethods = new WaitMethods();
+	
+	public  LogoutStepDefinition() {
+		super();
+		driver = Setup.driver;
+		PageFactory.initElements(driver, LogoutPageObject.class);
+	}
 	@Given("^je me déconnecter de mon espace Amazon avec succés$")
 	public void je_me_déconnecter_de_mon_espace_Amazon_avec_succés() throws Throwable {
 	   
@@ -13,13 +32,14 @@ public class LogoutStepDefinition {
 
 	@When("^je faire un mouse hoover sur le text \"([^\"]*)\"$")
 	public void je_faire_un_mouse_hoover_sur_le_text(String arg1) throws Throwable {
-	   
+		logoutPageObj.identifierVous();
 	}
 
 	@When("^je clique Déconnexion$")
 	public void je_clique_Déconnexion() throws Throwable {
-	    
-	}
+		logoutPageObj.logoutBtn();
+		
+		}
 
 	@Then("^se déconnécter et se rediriger vers la page login$")
 	public void se_déconnécter_et_se_rediriger_vers_la_page_login() throws Throwable {
