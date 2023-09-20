790c65a366b295b4f46d5317650c5458a361f218
SalhiFayza <fayzasalhif@gmail.com>
2023-09-20 11:36:58 +0100

    add classs logoutPageObject

--- src/test/java/logout/LogoutPageObject.java
+++ src/test/java/logout/LogoutPageObject.java
@@ -7,5 +7,39 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class LogoutPageObject {
-	
+	private WebDriver driver;
+
+	final static String BTN_IDENTIFIE = "nav-link-accountList-nav-line-1";
+
+	final static String BTN_SIGNOUT = "nav-item-signout";
+
+	final static String TITLE_PAGE_CONX = "//*[@id=\"authportal-main-section\"]/div[2]/div[2]/div[1]/form/div/div/div/h1";
+
+	// ******
+
+	@FindBy(how = How.ID, using = BTN_IDENTIFIE)
+	public static WebElement identifieVous;
+
+	@FindBy(how = How.ID, using = BTN_SIGNOUT)
+	public static WebElement signout;
+
+	@FindBy(how = How.XPATH, using = TITLE_PAGE_CONX)
+	public static WebElement verifPageConx;
+	  
+	public void identifieVousBtn() {
+		Actions action = new Actions(driver);
+		action.moveToElement(identifieVous).perform();
+		
+	}
+
+	public void signoutBtn() {
+
+		signout.click();
+	}
+
+	public String verifPageConx() {
+
+		return verifPageConx.getText();
+
+	}
 }
