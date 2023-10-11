005672c9b08028114d66c7ec877a701ee6ba2514
SalhiFayza <fayzasalhif@gmail.com>
2023-10-11 10:06:33 +0100

    update search product

--- src/test/java/searchProduct/SearchPageObject.java
+++ src/test/java/searchProduct/SearchPageObject.java
@@ -8,68 +8,75 @@ import org.openqa.selenium.support.How;
 public class SearchPageObject {
 
 	final static String CATEGORY_SELECT = "searchDropdownBox";
-	
+
 	final static String OPTION_CATEGORY = "//*[@id=\"searchDropdownBox\"]/option[27]";
-	
-	final static String  INPUT_SEARCH = "twotabsearchtextbox";
-	
+
+	final static String INPUT_SEARCH = "twotabsearchtextbox";
+
 	final static String ICON_SEARCH = "nav-search-submit-text";
-	
-	final static String NAME_PRODUCT = "//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div[2]/div[1]/h2/a/span";
-	
-	
-	@FindBy(how = How.ID, using = CATEGORY_SELECT )
+
+	final static String IMG_PRODUCT = "//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div[1]/span/a/div/img";
+
+	final static String NAME_PRODUCT = "title";
+
+
+	@FindBy(how = How.ID, using = CATEGORY_SELECT)
 	public static WebElement categorySelect;
-	
-	@FindBy(how = How.XPATH, using = OPTION_CATEGORY )
+
+	@FindBy(how = How.XPATH, using = OPTION_CATEGORY)
 	public static WebElement optionCategory;
-	
-	@FindBy(how = How.ID, using = INPUT_SEARCH )
+
+	@FindBy(how = How.ID, using = INPUT_SEARCH)
 	public static WebElement inputSearch;
-	
+
 	@FindBy(how = How.ID, using = ICON_SEARCH)
 	public static WebElement iconsearch;
-	
-	@FindBy(how = How.XPATH, using = NAME_PRODUCT)
+
+	@FindBy(how = How.XPATH, using = IMG_PRODUCT)
+	public static WebElement imgProduct;
+
+	@FindBy(how = How.ID, using = NAME_PRODUCT)
 	public static WebElement nameProduct;
-	
-	//
-	
-	public void category_Select() {
 		
+	public void category_Select() {
+
 		categorySelect.click();
 	}
-	
+
 	public void itemCategory() {
-		
+
 		optionCategory.click();
 	}
 
-	
 	public void inputNameProduct(String inputProduct) {
-		
+
 		inputSearch.sendKeys(inputProduct);
 	}
 
 	public void enterKeyBoard() {
 		// type enter with sendKeys method and pass Keys.ENTER
-				inputSearch.sendKeys(Keys.ENTER);
-				
+		inputSearch.sendKeys(Keys.ENTER);
+
 	}
-	
+
 	public void btnSearch() {
-		
+
 		iconsearch.click();
 	}
 	
-	
-	public String verifNameProduct() {
-		
-		String expectedMessage = nameProduct.getText();	
-		
-		return 	expectedMessage;
-		
+	public void clickImgProduct() {
+
+		imgProduct.click();
 	}
 	
-	
+	public String verifNameProduct() {
+
+		String expectedMessage = nameProduct.getText();
+
+		return expectedMessage;
+
+	}
+
+		
+
 }
